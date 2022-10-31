329d3d5d3188521d15e8053b46ea7664f0d041e5
saksham218 <sakshamkumar03218@gmail.com>
2022-11-01 00:20:42 +0530

    data imm

--- microprocessor.v
+++ microprocessor.v
@@ -2,6 +2,7 @@ module microprocessor(
     input wire ,
     output reg
 );
-    wire buffer;
+
+    assign data_in = data_imm?(ir_out[1]?{6'b111111,ir_out[1:0]}:{6'b000000,ir_out[1:0]}):(rf_mux ? acc_out : mbr_out);
     
 endmodule
