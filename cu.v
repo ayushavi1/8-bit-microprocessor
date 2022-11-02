bf8c868d53350de996d9fe80b6056fa76443917d
saksham218 <sakshamkumar03218@gmail.com>
2022-11-02 11:29:30 +0530

    immediate instructions

--- cu.v
+++ cu.v
@@ -6,7 +6,7 @@ module cu(
   output reg[1:0] select,
   output reg[7:0] state,
   output reg[1:0] RAM_in,
-  output reg MBR_we,IR_we,PC_inc,RF_we,Acc_we,MAR_we,RAM_we,ALU_mux,RF_mux, ALU_out_mux, MBR_mux, data_imm
+  output reg MBR_we,IR_we,PC_inc,RF_we,Acc_we,MAR_we,RAM_we,ALU_mux,RF_mux, ALU_out_mux, MBR_mux, data_imm,Acc_imm
 );
   reg [7:0]next_state;
   
@@ -34,6 +34,7 @@ module cu(
 				ALU_out_mux=0;
 				MBR_mux=0;
 				data_imm = 0;
+				Acc_imm=0;
 				RAM_in=2'b00;
 				mode=3'b111;
 				next_state=s1;
@@ -138,13 +139,19 @@ module cu(
 				data_imm = 1;
 				RF_we = 1;
 				Select = CU_in[3:2];
-				nextstate = s21;
+				nextstate = s0;
 			end
 		s8: // SUM
 			begin
 				Acc_we = 1;
 				ALU_out_mux = 1;
-				Select = CU_in[3:2];
+				Select = CU_in[1:0];
+				nextstate = s21;
+			end
+		s9: // SMI
+			begin
+				Acc_imm = 1;
+				Acc_we = 1;
 				nextstate = s21;
 			end
 		s20: // From S6 (MR)
@@ -158,22 +165,35 @@ module cu(
 			end
 		s21: // From S8 (SUM)
 			begin
+				Acc_imm=0;
 				Acc_we = 1;
 				ALU_mux = 1;
 				ALU_out_mux = 0;
-				Select = CU_in[1:0];
+				Select = CU_in[3:2];
 				case(CU_in[7:4])
 					4'b0100: // SUM
 						mode = 3'b000;
+					4'b1100: // SMI
+						mode = 3'b000;
 					4'b0101: // SB
 						mode = 3'b001;	
+					4'b1101: // SBI
+						mode = 3'b001;
 					4'b0111: // CM
+						mode = 3'b010;
+					4'b1111: // CMI
 						mode = 3'b010;
 					4'b0110: // ANR
 						mode = 3'b011;
+					4'b1110: // ANI
+						mode = 3'b011;
 					4'b1000: // ORR
 						mode = 3'b100;
+					4'b1001: // ORI
+						mode = 3'b100;
 					4'b1010: // XRR
+						mode = 3'b101;
+					4'b1011: // XRI
 						mode = 3'b101;
 				endcase
 				nextstate = s22;
--- microprocessor.v
+++ microprocessor.v
@@ -4,5 +4,6 @@ module microprocessor(
 );
 
     assign data_in = data_imm?(ir_out[1]?{6'b111111,ir_out[1:0]}:{6'b000000,ir_out[1:0]}):(rf_mux ? acc_out : mbr_out);
+	assign acc_in = acc_imm?(ir_out[1]?{6'b111111,ir_out[1:0]}:{6'b000000,ir_out[1:0]}):(alu_out_mux ? data_out : z);
     
 endmodule
